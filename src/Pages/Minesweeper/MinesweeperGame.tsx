96b2448d666f3ae2e89ee39555bf27e9fc8c7784
Magthylius <56510076+Magthylius@users.noreply.github.com>
2023-05-21 21:32:38 +0800

    feat: field data

--- src/Pages/Minesweeper/MinesweeperGame.tsx
+++ src/Pages/Minesweeper/MinesweeperGame.tsx
@@ -1,26 +1,27 @@
 import { useState } from "react";
-import { FieldProps, TileProps } from "./MinesweeperProps";
+import { FieldProps, TileData, TileProps } from "./MinesweeperProps";
 import { Vector2 } from "../../HeaderInterfaces";
 import "./MinesweeperGame.scss"
 
 function Tile(props: TileProps) {
-  const value = props.value;
+  const value = props.tileData.value;
   const display = value <= 0 ? "" : value;
   return (
-    <button className="tile">{value}</button>
+    <button className="tile" onClick={props.onTileClicked}>{value}</button>
   );
 }
 
 function Field(props: FieldProps) {
   const fieldSize: Vector2 = props.fieldSize;
-  const fieldData: TileProps[][] = props.fieldData;
+  const fieldData: TileData[][] = props.fieldData;
 
-  const rowTiles = fieldData.map((rowData: TileProps[], index) => {
-    const tiles = rowData.map((tile: TileProps, index) => {
-      return <Tile value={tile.value} isOpened={tile.isOpened} />;
+  const rowTiles = fieldData.map((rowData: TileData[], rowIndex) => {
+    const tiles = rowData.map((tileData: TileData, columnIndex) => {
+      return <Tile key={columnIndex} tileData={tileData} onTileClicked={() => props.onTileClicked(rowIndex, columnIndex)} />;
     });
+
     return (
-      <div>
+      <div key={rowIndex} className="fieldRow">
         {tiles}
       </div>
     );
@@ -35,12 +36,19 @@ function Field(props: FieldProps) {
 
 export default function MinesweeperGame() {
   const [fieldSize, setFieldSize] = useState<Vector2>({ x: 10, y: 10 });
-  const [fieldData, setFieldData] = useState<TileProps[][]>([Array(fieldSize.x * fieldSize.y).fill({ value: 0, isOpened: false })]);
+  const [fieldData, setFieldData] =
+    useState<TileData[][]>(Array(fieldSize.y).fill(0).map(row => new Array(fieldSize.x).fill({ value: -1, isOpened: false })));
+
+  function handleOnTileClicked(x: number, y: number) {
+    const newFieldData = fieldData.slice(0, fieldData.length);
+    newFieldData[x][y] = { ...newFieldData[x][y], value: 0, isOpened: true }
+    setFieldData(newFieldData);
+  }
 
   return (
     <div>
       <div id="spacing"></div>
-      <Field fieldSize={fieldSize} fieldData={fieldData}></Field>
+      <Field fieldSize={fieldSize} fieldData={fieldData} onTileClicked={handleOnTileClicked} />
     </div>
   );
 }
--- src/Pages/Minesweeper/MinesweeperProps.tsx
+++ src/Pages/Minesweeper/MinesweeperProps.tsx
@@ -1,11 +1,17 @@
 import { Vector2 } from "../../HeaderInterfaces";
 
-export interface TileProps {
+export interface TileData {
   value: number;
-  isOpened: false;
+  isOpened: boolean;
+}
+
+export interface TileProps {
+  tileData: TileData,
+  onTileClicked: () => void;
 }
 
 export interface FieldProps {
   fieldSize: Vector2;
-  fieldData: TileProps[][];
+  fieldData: TileData[][];
+  onTileClicked: (x: number, y: number) => void;
 }
